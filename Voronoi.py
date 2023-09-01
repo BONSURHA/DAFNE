d3ab79eb8a87611aedc504956fb62671c3633f8d
BONSURHA <nicolobossi01@gmail.com>
2023-09-01 18:54:43 +0200

    changes

--- Voronoi.py
+++ Voronoi.py
@@ -1,46 +1,101 @@
 from PIL import Image, ImageDraw
-from urllib.request import urlopen
 import math
 import random
+from urllib.request import urlopen
 
-# Funzione per calcolare la cella Voronoi per un punto
-# Restituisce l'elenco dei punti all'interno della cella
-def calculate_voronoi_cell(point, sites):
-    voronoi_cell = []
-    for x in range(point[0] - 1, point[0] + 2):
-        for y in range(point[1] - 1, point[1] + 2):
-            voronoi_cell.append((x, y))
-    return voronoi_cell
+
+# Funzione per calcolare la distanza euclidea tra due punti
+def euclidean_distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+
+# Funzione per trovare la cella Voronoi più vicina a un punto
+def find_closest_cell(point, sites):
+    closest_site = None
+    min_distance = float('inf')
+
+    for site in sites:
+        distance = euclidean_distance(point, site)
+        if distance < min_distance:
+            min_distance = distance
+            closest_site = site
+    return closest_site
+
+
+# creare una tassellazione Voronoi
+def create_voronoi(width, height, num_sites):
+    # sites = [(random.randint(0, width), random.randint(0, height)) for _ in range(num_sites)]
+    sites = [(random.uniform(0, width), random.uniform(0, height)) for _ in range(num_sites)]
+
+    voronoi_cells = {site: [] for site in sites}
+
+    for x in range(width):
+        for y in range(height):
+            point = (x, y)
+            closest_site = find_closest_cell(point, sites)
+            voronoi_cells[closest_site].append(point)
+
+    return voronoi_cells
 
 
 def calculate_num_sites(width, height):
     return int(math.sqrt(width * height))
 
 
+# Funzione per unire casualmente i tasselli adiacenti
+def merge_adjacent_cells(voronoi_cells, num_merges):
+    for _ in range(num_merges):
+        site1, site2 = random.sample(list(voronoi_cells.keys()), 2)
+
+        # Unisci le celle dei siti site1 e site2
+        voronoi_cells[site1] += voronoi_cells[site2]
+        del voronoi_cells[site2]
+
+
+# Funzione per creare un'immagine del frammento con dimensioni adattate alla cella
+def create_fragment_image(fragment_cell, input_image):
+    min_x = min(point[0] for point in fragment_cell)
+    max_x = max(point[0] for point in fragment_cell)
+    min_y = min(point[1] for point in fragment_cell)
+    max_y = max(point[1] for point in fragment_cell)
+
+    fragment_width = max_x - min_x + 1
+    fragment_height = max_y - min_y + 1
+
+    fragment_image = Image.new("RGBA", (fragment_width, fragment_height))
+    draw = ImageDraw.Draw(fragment_image)
+
+    for point in fragment_cell:
+        x, y = point
+        pixel = input_image.getpixel((x, y))
+        draw.point((x - min_x, y - min_y), fill=pixel)
+
+    return fragment_image
+
+
 # Funzione per suddividere l'immagine in frammenti e salvarli uno a uno
-def split_and_save_fragments(url, output_directory):
+def split_and_save_fragments(url, output_directory, num_merges):
     input_image = Image.open(urlopen(url))
     width, height = input_image.size
     num_sites = calculate_num_sites(width, height)
+    print(width)
+    print(height)
+    print(num_sites)
 
-    sites = [(random.randint(0, width), random.randint(0, height)) for _ in range(num_sites)]
+    voronoi_cells = create_voronoi(width, height, num_sites)
 
-    for site in sites:
-        voronoi_cell = calculate_voronoi_cell(site, sites)
-        fragment_image = Image.new("RGB", (3, 3))
-        draw = ImageDraw.Draw(fragment_image)
+    # Unisci casualmente i tasselli adiacenti prima di creare i frammenti
+    merge_adjacent_cells(voronoi_cells, num_merges)
 
-        for point in voronoi_cell:
-            x, y = point
-            if 0 <= x < width and 0 <= y < height:
-                pixel = input_image.getpixel((x, y))
-                draw.point((x - site[0] + 1, y - site[1] + 1), fill=pixel)
-
+    for site, fragment_cell in voronoi_cells.items():
+        fragment_image = create_fragment_image(fragment_cell, input_image)
         fragment_image.save(f"{output_directory}/fragment_{site}.png")
+
 
 # Esempio di utilizzo
 if __name__ == "__main__":
-    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Leonardo_Magi.jpg/600px-Leonardo_Magi.jpg"
     output_directory = "output"
+    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Leonardo_Magi.jpg/600px-Leonardo_Magi.jpg"
+    num_merges = 10  # Numero di unione casuale dei tasselli adiacenti
 
-    split_and_save_fragments(url, output_directory)
+    split_and_save_fragments(url, output_directory, num_merges)
